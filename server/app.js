dbe06d5a316b80f18087c4715bdd1f00f6674f53
Borys Ushakov <inlvert@gmail.com>
2024-07-29 15:28:07 +0300

    add connection basicErrorHendler

--- server/app.js
+++ server/app.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const cors = require("cors");
+const basicErrorHendler = require("./middlewares/errors/basic");
+
+const router = require('./routers')
 
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(router);
+app.use(basicErrorHendler);
 
 
 module.exports = app;
