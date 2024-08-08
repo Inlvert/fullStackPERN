c1d1e9bfe2e979853e65f90ae26542f76f8ed709
Borys Ushakov <inlvert@gmail.com>
2024-08-08 22:03:42 +0300

    add token mw

---
+++ server/middlewares/token.mw.js
@@ -0,0 +1,48 @@
+const createHttpError = require("http-errors");
+const JwtService = require('../services/jwt.service');
+const { RefreshToken } = require("../models");
+
+
+module.exports.cheackAccessToken = async (req, res, next) => {
+  try {
+    const {headers: {athorization}} = req;
+
+    if(!athorization) {
+      return next(createHttpError(401, 'Access token required'))
+    }
+
+    const [type, token] = athorization.split(' ');
+
+    const tokenPayload = await JwtService.verifyAccessToken(token)
+
+    req.tokenData = tokenPayload;
+
+  } catch (error) {
+    next(error)
+  }
+}
+
+module.exports.cheackRefreshToken = async (req, res, next) => {
+  try {
+    const {body: {refreshToken}} = req;
+
+    if(!refreshToken) {
+      return next(createHttpError(400, 'Refresh token required'))
+    }
+
+    const tokenPayload = await JwtService.verifyRefreshToken(refreshToken);
+
+    const tokenInstance = await RefreshToken.findOne({ where: { token: refreshToken, userId: tokenPayload.id } })
+
+    if(! tokenInstance) {
+      return next(createHttpError(404, 'Refresh token not found'))
+    }
+
+    req.tokenInstance = tokenInstance;
+
+    next();
+
+  } catch (error) {
+    next(error)
+  }
+}
