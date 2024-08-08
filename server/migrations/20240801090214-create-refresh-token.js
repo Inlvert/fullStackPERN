a8d4faf3929b05b5094525256c1951bb27936d9b
Borys Ushakov <inlvert@gmail.com>
2024-08-08 21:59:10 +0300

    create model + migration refresh token

---
+++ server/migrations/20240801090214-create-refresh-token.js
@@ -0,0 +1,41 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('refresh_tokens', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      token: {
+        type: Sequelize.STRING,
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        field: "user_id",
+        references: {
+          model: "users",
+          key: "id",
+        },
+        onDelete: "CASCADE",
+        onUpdate: "CASCADE",
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        field: "created_at",
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        field: "updated_at",
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('refresh_tokens');
+  }
+};
