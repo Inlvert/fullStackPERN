8eb5efb1442d453af1105db504d7503f54c07198
Borys Ushakov <inlvert@gmail.com>
2024-08-19 15:37:28 +0300

    connect cart reducer

--- client/src/redux/index.js
+++ client/src/redux/index.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import productsReducer from "./slice/productSlice";
 import authReducer from "./slice/authSlice";
+import cartReducer from "./slice/cartSlice";
 
 const store = configureStore({
   reducer: {
     product: productsReducer,
     auth: authReducer,
+    cart: cartReducer,
   },
 });
 
