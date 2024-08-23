107871cab6f6caaa297b668006f16f03c9379fc7
Borys Ushakov <inlvert@gmail.com>
2024-08-23 22:53:13 +0300

    add change for header comp.

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -1,8 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./style.css";
+import { useDispatch, useSelector } from "react-redux";
+import Svg from "./shopping-cart.svg";
+import { logout } from "../../redux/slice/authSlice";
+import { clearTokens } from "../../api";
 
 const Header = (props) => {
+  const { user } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+
+  const hendleLogout = () => {
+    clearTokens();
+    dispatch(logout());
+  };
+
   return (
     <header>
       <nav className="textRow">
@@ -13,8 +25,15 @@ const Header = (props) => {
           <Link to="/login" className="link">
             <li>Login</li>
           </Link>
+          <li>
+            <text style={{ color: 'white'}}>Hello  {user ? `${user.name}` : "guest"}</text>
+            <button onClick={hendleLogout}>logout</button>
+          </li>
           <Link to="/cart" className="link">
-            <li>Cart</li>
+            <li>
+              <img src={Svg} alt="cart" />
+              Cart
+            </li>
           </Link>
         </ul>
       </nav>
